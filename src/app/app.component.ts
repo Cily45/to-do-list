6e698e26217c3d8be19928f2fdaeea49bcfbff20
cilya <cecile.fischer@coda-student.school>
2025-03-01 16:56:33 +0100

    correction add todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,18 @@ import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Todo} from './models/Todo';
 import {TodoComponent} from './todo/todo.component';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TodoComponent],
+  imports: [RouterOutlet, TodoComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'todolist';
   todos!: Todo[];
-  value!: string;
+  value: string = "";
 
 
   ngOnInit(): void {
@@ -20,9 +21,10 @@ export class AppComponent implements OnInit {
     this.todos.push(new Todo("fdsfsd"));
   }
 
-  addTodo(value: string){
-    if(value.length > 0){
-      this.todos.push(new Todo(value));
+  addTodo(): void{
+    if(this.value.length > 0){
+      this.todos.push(new Todo(this.value));
+      this.value = "";
     }
   }
 }
